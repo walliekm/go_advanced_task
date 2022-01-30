6a2f8782f22a1382f1eb0767329f1c2ecb418b05
Thiniki <cgh43@qq.com>
2022-01-30 22:09:33 +0800

    第一周作业

--- week1/dao/user.go
+++ week1/dao/user.go
@@ -1,11 +1,26 @@
 package dao
 
+import (
+	"database/sql"
+
+	"github.com/pkg/errors"
+)
+
 type UserInfo struct {
 	Id     int
 	Name   string
 	Passwd string
 }
 
-func (this *mysqlDAO) GetUserInfoByName(name string) (uInfo *UserInfo, err error) {
-	return
+func (this *sqlDAO) GetUserInfoByName(name string) (*UserInfo, error) {
+	var uInfo UserInfo
+	if err := this.db.QueryRow("SELECT id,name,passwd FROM user WHERE name=?", name).Scan(&uInfo.Id, &uInfo.Name, &uInfo.Passwd); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, ErrDataNotFound
+		}
+
+		return nil, errors.Wrap(err, "GetUserInfoByName fail")
+	}
+
+	return &uInfo, nil
 }
--- week1/main.go
+++ week1/main.go
@@ -4,29 +4,26 @@ import (
 	"fmt"
 	"log"
 	"week1/dao"
-
-	"github.com/pkg/errors"
 )
 
 func main() {
-	sqlDAO, err := dao.NewMySQLDao("localhost", 3306, "test", "passwd")
+	sqlDAO, err := dao.NewMySQLDao("localhost", 3306, "test", "passwd", "demo_database")
 	if err != nil {
 		//数据库无法连接，属于不可恢复的异常，触发panic
 		panic(err)
 	}
 
-	name := "foo"
-	uInfo, err := sqlDAO.GetUserInfoByName(name)
+	uInfo, err := sqlDAO.GetUserInfoByName("demo")
 	if err != nil {
 		//找不到指定的数据
 		//此时根据业务需要，如果不影响后续逻辑，则应忽略此错误，然后继续处理后续逻辑
 		//如果会影响到后续逻辑，则提前结束，返回结果，本段代码假设为后者进行处理
-		if errors.Cause() == dao.ErrDataNotFound {
+		if err == dao.ErrDataNotFound {
 			fmt.Printf("user %s is not existed")
 			return
 		}
 
-		//其它错误，记录日志并显示错误，然后结束
+		//其它错误，记录日志并返回错误，然后结束
 		log.Printf("dao query fail:%+v", err)
 		fmt.Printf("Server internal error")
 		return
